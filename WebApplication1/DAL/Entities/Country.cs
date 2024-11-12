3d4ef9a6a13d717184adaa86b3651db1bf84231a
steven barriga <stevenbarriga45@gmail.com>
2024-11-12 13:18:44 -0500

    creacion base de datos

---
+++ WebApplication1/DAL/Entities/Country.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.DAL.Entities
+{
+    public class Country : AuditBase
+    {
+        [Display (Name = "Pais")]// para identificarlo mas facil
+        [MaxLength(50, ErrorMessage = "El campo debe tener como mazimo 5o caracteres")]//maximo longitud
+        [Required (ErrorMessage = "El campo {0} es obligstorio")]
+        public string Name { get; set; }
+
+    }
+    public class City : AuditBase
+    {
+        [Display(Name = "Ciudad")]// para identificarlo mas facil
+        [MaxLength(50, ErrorMessage = "El campo debe tener como mazimo 5o caracteres")]//maximo longitud
+        [Required(ErrorMessage = "El campo {0} es obligstorio")]
+        public string Name { get; set; }
+
+    }
+}

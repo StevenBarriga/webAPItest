d20954bd944c5872d7e6982bb0df7fa73999a597
steven barriga <stevenbarriga45@gmail.com>
2024-11-14 13:44:52 -0500

    actualizacion

--- WebApplication1/DAL/Entities/Country.cs
+++ WebApplication1/DAL/Entities/Country.cs
@@ -10,12 +10,5 @@ namespace WebApplication1.DAL.Entities
         public string Name { get; set; }
 
     }
-    public class Estate : AuditBase
-    {
-        [Display(Name = "Ciudad")]// para identificarlo mas facil
-        [MaxLength(50, ErrorMessage = "El campo debe tener como mazimo 5o caracteres")]//maximo longitud
-        [Required(ErrorMessage = "El campo {0} es obligstorio")]
-        public string Name { get; set; }
-
-    }
+    
 }

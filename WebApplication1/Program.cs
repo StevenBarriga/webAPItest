be0852d0c77ba37507b04fe257845b3be5ddd738
steven barriga <stevenbarriga45@gmail.com>
2024-11-14 19:52:47 -0500

    seeder

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -14,12 +14,28 @@ builder.Services.AddDbContext<DataBaseContext>(options => options.UseSqlServer(b
 
 //Contenedor de dependencias hecho el 13/11/2024
 builder.Services.AddScoped<ICountryService, CountryService>();
+//////////////////////////////////////////////////builder.Services.AddScoped<IStateService, StateService> ();
+builder.Services.AddTransient<SeederDB>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+SeederData();
+void SeederData()
+{
+    IServiceScopeFactory? scopeFactory = app.Services.GetService<IServiceScopeFactory?>();
+
+    using (IServiceScope? scope = scopeFactory.CreateScope())
+    {
+        SeederDB? service = scope.ServiceProvider.GetService<SeederDB?>();
+        service.SeederAsync().Wait();
+    }
+}
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
